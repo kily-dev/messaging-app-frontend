bcc79f2f031c47b5449b0387dc594db999b705c7
Ilyass Kaddani <k.ilyass-dev@proton.me>
2026-04-04 14:08:14 +0100

    chore: Installed TailwindCSS

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import MessagesProvider from "./providers/MessagesProvider.tsx";
 import ChannelsProvider from "./providers/ChannelsProvider.tsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
